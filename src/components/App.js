ad4c0b8441441a4a4d9b4cc1290035a86e8a7a40
Ravi Singh <ravi08371@gmail.com>
2022-11-22 00:03:52 +0530

    did some more changes

--- src/components/App.js
+++ src/components/App.js
@@ -28,7 +28,7 @@ const App = () => {
               <li>Tutorials by industry experts</li>
               <li>Peer & expert code review</li>
               <li>Coding exercises</li>
-              <li>Access yo our Github repos</li>
+              <li>Access your GitHub repos</li>
               <li>Community forum</li>
               <li>Flashcard decks</li>
             </ul>
